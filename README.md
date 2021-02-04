d208e694cb5bdc91be07454f7fcf43c4691f2b30
MarioKrausser <66949675+MarioKrausser@users.noreply.github.com>
2021-02-04 12:55:51 +0100

    Add files via upload
    Project Setup

---
+++ README.md
@@ -0,0 +1,21 @@
+# Webpack Express Example App
+
+## Project Motivation
+
+The motive of this project is to have you a taste of the environment and tools you will most likely come across in a front end role. 
+Your focus should be to understand the role every tool and technology is playing in the overall architecture. 
+You shouldn’t feel the need to memorize the particular commands, config setups, or structure that we create here. 
+Every projetc in the industry will have its own custom setup, but if you understand the moving pieces, 
+you will be able to get the gist of even far more complicated projects than this one.
+
+## Project Introduction - What You Will Build
+
+We will build a web tool that allows users to run Natural Language Processing (NLP) on articles or blogs found on other websites. 
+NLP is the ability of an application to understand the human language, written or oral.
+
+##Installation
+
+* clone from GitHub the directory
+* run npm install or npm i to install dependencies
+* run npm start-prod to start up the server
+* now you can open localhost:8000 
---
+++ webpack.prod.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+//Define dotenv
+const dotenv = require('dotenv');
+const config = dotenv.config().parsed;
+
+console.log(config)
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    node: {
+        fs: 'empty',
+        net: 'empty',
+    },
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new webpack.DefinePlugin({
+            'process.env': JSON.stringify(config),
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+
+        new WorkboxPlugin.GenerateSW(),
+    ]
+}
