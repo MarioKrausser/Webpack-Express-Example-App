cadc0f4d13788653b6e4f418c92897d47878bdd8
MarioKrausser <66949675+MarioKrausser@users.noreply.github.com>
2021-02-04 13:21:20 +0100

    Add files via upload

---
+++ js/urlChecker.js
@@ -0,0 +1,16 @@
+function checkForUrl(inputText) {
+    console.log("::: Running checkForUrl :::", inputText);
+    let names = [
+        "Picard",
+        "Janeway",
+        "Kirk",
+        "Archer",
+        "Georgiou"
+    ]
+
+    if(names.includes(inputText)) {
+        alert("Welcome, Captain!")
+    }
+}
+
+export { checkForUrl }
